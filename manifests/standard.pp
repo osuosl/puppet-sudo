06f385879851b25b8e83a30bac5a85e0a0b2c9c4
Mike Cooper <mythmon@osuosl.org>
2011-02-09 15:00:24 -0800

    Add a standard sudo config. Not default, just a easy baseline.

---
+++ manifests/standard.pp
@@ -0,0 +1,22 @@
+# Class: sudo::standard
+#
+# Sets up a fairly normal set of sudo rules.
+#
+# Usage:
+#   # 'include sudo' is not needed
+#   include sudo::standard
+#
+class sudo::standard inherits sudo {
+    # Also sets up all the packages and files for sudo.
+
+    # Enable environemnt resets.
+    sudo::default { 'env_reset':
+        option => 'env_reset',
+    }
+
+    # Give root and %wheel sudo access.
+    sudo::sudoer {
+        "root":;
+        "%wheel":
+    }
+}
